3114a8f1e813d7d4fb1cc138e2c441d05c714b75
unknown <jh2000lee@nate.com>
2021-09-29 23:04:40 +0700

    [FIX]채널 정보 불러올 때 채널 id 동일 이슈 수정 (getAllIndexes 함수로 해결)

--- src/components/card/VideoInfoCard.jsx
+++ src/components/card/VideoInfoCard.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
+import React from "react";
 
 // CSS
-import style from './thumbnailCard.module.css';
+import style from "./thumbnailCard.module.css";
 
 function VideoInfoCard(props) {
-    console.log(props);
-    const { videoInfo } = props;
-    const cardSize = {
-        width: videoInfo.snippet.thumbnails.medium.width,
-        // height: videoInfo && videoInfo.snippet.thumbnails.medium.height,
-    };
-    return (
-        <>
-            <div className={style.cardWrap} style={cardSize}>
-                <img
-                    src={videoInfo.snippet.thumbnails.medium.url}
-                    width={videoInfo.snippet.thumbnails.medium.width}
-                    height={videoInfo.snippet.thumbnails.medium.height}
-                ></img>
-                <div>{videoInfo.snippet.title}</div>
-                <div>
-                    <img
-                        src={videoInfo.snippet.channelThumbnails.default.url}
-                        width={videoInfo.snippet.channelThumbnails.default.width}
-                        height={videoInfo.snippet.channelThumbnails.default.height}
-                    ></img>
-                    {videoInfo.snippet.channelTitle}
-                </div>
-            </div>
-        </>
-    );
+  const { videoInfo } = props;
+  const cardSize = {
+    width: videoInfo.snippet.thumbnails.medium.width,
+    // height: videoInfo && videoInfo.snippet.thumbnails.medium.height,
+  };
+  return (
+    <>
+      <div className={style.cardWrap} style={cardSize}>
+        <img
+          src={videoInfo.snippet.thumbnails.medium.url}
+          width={videoInfo.snippet.thumbnails.medium.width}
+          height={videoInfo.snippet.thumbnails.medium.height}
+        ></img>
+        <div>{videoInfo.snippet.title}</div>
+        <div>
+          <img
+            src={videoInfo.snippet.channelThumbnails.default.url}
+            width={videoInfo.snippet.channelThumbnails.default.width}
+            height={videoInfo.snippet.channelThumbnails.default.height}
+          ></img>
+          {videoInfo.snippet.channelTitle}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default VideoInfoCard;
--- src/page/MostPopularList.jsx
+++ src/page/MostPopularList.jsx
@@ -1,76 +1,92 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // COMPONENT
-import ThumbnailCard from '../components/card/VideoInfoCard';
+import ThumbnailCard from "../components/card/VideoInfoCard";
 
 // SERVICE
-import apiService from '../service/apiService';
+import apiService from "../service/apiService";
 
 // 인기 목록
 function MostPopularList(props) {
-    const [popularList, setPopularList] = useState([]); // 인기 목록 + 채널 썸네일
-    let videoInfos = []; // 인기 목록 임시 저장
+  const [popularList, setPopularList] = useState([]); // 인기 목록 + 채널 썸네일
 
-    // 1. 첫 랜더링 시 목록 불러오기
-    useEffect(() => {
-        getMostPopularList();
-    }, []);
+  // 1. 첫 랜더링 시 목록 불러오기
+  useEffect(() => {
+    getMostPopularList();
+  }, []);
 
-    // 2. 인기 목록 불러오기
-    function getMostPopularList() {
-        const filter = {
-            part: 'snippet',
-            chart: 'mostPopular',
-            maxResults: 15,
-            regionCode: 'KR',
-        };
-        apiService.getMostPopularList(filter).then((res) => {
-            videoInfos = res.data.items; // api 호출하여 받은 인기목록 videoInfos 변수에 임시 저장
-            getChannelsInfo(); // 인기목록의 채널 id를 이용하여 채널 정보 불러오기
+  // 2. 인기 목록 불러오기
+  function getMostPopularList() {
+    const filter = {
+      part: "snippet",
+      chart: "mostPopular",
+      maxResults: 50,
+      regionCode: "KR",
+    };
+    apiService.getMostPopularList(filter).then((res) => {
+      let videoInfos = []; // 인기 목록
+      videoInfos = res.data.items; // api 호출하여 받은 인기목록 videoInfos 변수에 저장
+      getChannelsInfo(videoInfos); // 인기목록의 채널 id를 이용하여 채널 정보 불러오기
+    });
+  }
+
+  // 3. 채널 정보 불러오기
+  function getChannelsInfo(videoInfos) {
+    // 채널 id를 arr 형태로 보내줘야함
+    const channelIdArr = videoInfos.map((item) => {
+      return item.snippet.channelId;
+    });
+    console.log("channelIdArr", channelIdArr);
+
+    const filter = {
+      part: "snippet",
+      maxResults: 50,
+      id: channelIdArr,
+    };
+
+    apiService.getChannelsInfo(filter).then((res) => {
+      // indexOf를 이용하여 인기 목록의 순서로 정렬해주기
+      let newChannelsInfoList = [];
+      res.data.items.forEach((item, index) => {
+        const indexOfAll = getAllIndexes(channelIdArr, item.id);
+        console.log(indexOfAll);
+        indexOfAll.forEach((indexOf) => {
+          newChannelsInfoList[indexOf] = item;
         });
+      });
+      // 인기 목록 + 채널 썸네일 state에 저장 할 새로운 arr 생성
+      const newPopularList = videoInfos.map((item, index) => {
+        item.snippet.channelThumbnails =
+          newChannelsInfoList[index].snippet.thumbnails;
+        return item;
+      });
+      setPopularList(newPopularList); // 인기 목록 + 채널 썸네일 state에 저장
+    });
+  }
+
+  // indexOf 로는 동일 채널 id 일때 문제 발생-->  indexOfAll 함수가 필요함
+  function getAllIndexes(arr, val) {
+    var indexes = [],
+      i = -1;
+    while ((i = arr.indexOf(val, i + 1)) != -1) {
+      indexes.push(i);
     }
+    return indexes;
+  }
 
-    // 3. 채널 정보 불러오기
-    function getChannelsInfo() {
-        // 채널 id를 arr 형태로 보내줘야함
-        const channelIdArr = videoInfos.map((item) => {
-            return item.snippet.channelId;
-        });
-        console.log('channelIdArr', channelIdArr);
-
-        const filter = {
-            part: 'snippet',
-            maxResults: 15,
-            id: channelIdArr,
-        };
-        apiService.getChannelsInfo(filter).then((res) => {
-            // indexOf를 이용하여 인기 목록의 순서로 정렬해주기
-            let newChannelsInfoList = [];
-            res.data.items.forEach((item, index) => {
-                console.log('index', index);
-                console.log('item.id', item.id);
-                console.log('channelIdArr.indexOf(itme.id)', channelIdArr.indexOf(item.id));
-                newChannelsInfoList[channelIdArr.indexOf(item.id)] = item;
-                // newChannelsInfoList.push(res.data.items[channelIdArr.indexOf(item.id)]);
-            });
-            console.log('newChannelsInfoList', newChannelsInfoList);
-            // 인기 목록 + 채널 썸네일 state에 저장 할 새로운 arr 생성 // TODO 채널 정보 불러올 때 채널 id 동일 이슈 발생
-            const newPopularList = videoInfos.map((item, index) => {
-                item.snippet.channelThumbnails = newChannelsInfoList[index].snippet.thumbnails;
-                return item;
-            });
-            setPopularList(newPopularList); // 인기 목록 + 채널 썸네일 state에 저장
-        });
-    }
-
-    return (
-        <>
-            {popularList.length > 0 &&
-                popularList.map((videoInfo, index) => {
-                    return <ThumbnailCard key={videoInfo.id} videoInfo={videoInfo}></ThumbnailCard>;
-                })}
-        </>
-    );
+  return (
+    <>
+      {popularList.length > 0 &&
+        popularList.map((videoInfo, index) => {
+          return (
+            <ThumbnailCard
+              key={videoInfo.id}
+              videoInfo={videoInfo}
+            ></ThumbnailCard>
+          );
+        })}
+    </>
+  );
 }
 
 export default MostPopularList;

87f58696c854e018d2ed24bd5e8ec2676d56e3c8
이재홍사원 <jh2000lee@nate.com>
2021-12-01 14:56:10 +0900

    [ADD] 좌측 메뉴 활성화 기본 레이아웃 작업

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import style from './header.module.css';
 import { useHistory } from 'react-router-dom';
 
@@ -6,10 +6,14 @@ import { useHistory } from 'react-router-dom';
 function Header() {
     const history = useHistory();
     const keyWord = useRef(); // ref 를 이용한 keyWord 관리
+    const menuAsideWrap = useRef(); // 왼쪽 사이드 메뉴 wrap
+    const menuAside = useRef(); // 왼쪽 사이드 메뉴
+    const menuAsideBackground = useRef(); // 왼쪽 사이드 메뉴 뒷 배경
 
+    const [menuOpen, setMenuOpen] = useState(false); // 사이드 메뉴 오픈 여부
     const [searchCount, setSearchCount] = useState(0); // 검색 횟수 (같은 키워드로 검색 시 location.search 변화를 주기 위함) --> TODO 유지 할지 지울지 고민해보기
 
-    // 검색 시 searchList로 이동 --> 쿼리를 이용 하여 파라미터 보내주기
+    // 1. 검색 시 searchList로 이동 --> 쿼리를 이용 하여 파라미터 보내주기
     function search() {
         if (keyWord.current.value == (null || '')) return;
         history.push(`/searchList?q=${keyWord.current.value}&searchCount=${searchCount}`);
@@ -17,15 +21,30 @@ function Header() {
         setSearchCount(searchCount + 1);
     }
 
-    // 로고 클릭 시 인기 목록으로 이동
+    // 2. 로고 클릭 시 인기 목록으로 이동
     function goToMostPopularList() {
         history.push(`/mostPopularList`);
     }
+
+    // 3. 메뉴 버튼 클릭 시 사이드 메뉴 활성화
+    function clickMenuBtn() {
+        setMenuOpen(!menuOpen);
+    }
+    // menuOpen 에 따른 분기처리
+    useEffect(() => {
+        if (menuOpen == true) {
+            menuAsideWrap.current.style.opacity = 1;
+            menuAsideWrap.current.style.pointerEvents = 'auto';
+        } else {
+            menuAsideWrap.current.style.opacity = 0;
+            menuAsideWrap.current.style.pointerEvents = 'none';
+        }
+    }, [menuOpen]);
     return (
         <>
             <header className={style.wrap}>
                 <div className={style.headerLeft}>
-                    <img className={style.menuBtn} src="/images/menu.png" />
+                    <img className={style.menuBtn} src="/images/menu.png" onClick={clickMenuBtn} />
                     <img className={style.logo} src="/images/logo.png" onClick={goToMostPopularList} />
                 </div>
                 <div className={style.headerCenter}>
@@ -49,7 +68,12 @@ function Header() {
                     <div>아이템2</div>
                 </div>
             </header>
-            {/* <aside>사이드 메뉴 활성화</aside> */}
+            <aside ref={menuAsideWrap} className={style.menuAsideWrap}>
+                <div ref={menuAside} className={style.menuAside}>
+                    사이드 메뉴 활성화
+                </div>
+                <div ref={menuAsideBackground} className={style.menuAsideBackground} onClick={clickMenuBtn}></div>
+            </aside>
         </>
     );
 }

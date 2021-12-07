d2a5cf6e745cd68629d2fc8ef1997d32b98fb524
이재홍사원 <jh2000lee@nate.com>
2021-12-07 13:31:58 +0900

    [ADD] pathname 변경 시 스크롤 최상단으로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // CSS
-import "./globalStyles.css"; // 전역 스타일 CSS
+import './globalStyles.css'; // 전역 스타일 CSS
 
 // LAYOUT
-import MainLayout from "./layout/MainLayout"; // 메인 레이아웃
+import ScrollToTop from './util/ScrollToTop'; // 스크롤 상단으로 이동
+import MainLayout from './layout/MainLayout'; // 메인 레이아웃
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/">
-            <MainLayout />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/">
+                        <ScrollToTop />
+                        <MainLayout />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
